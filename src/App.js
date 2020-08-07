84b7c43a39f8a7ae04bf585958a6cd67da40bbff
Nstampfli <nathan.stampfli@hetic.net>
2020-08-07 18:11:58 +0200

    add blurTop effet, but comment while waiting to find the right style to make it beautiful

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage/Home.page';
-import ProjectPage from './pages/ProjectPage/Project.page';
+import HomePage from './pages/Home.page';
+import ProjectPage from './pages/Project.page';
+
+// import Blur from './components/blurTop.component';
 
 function App() {
   return (
   <Router>
       <div className='App'>
-      <Switch>
+        {/* <Blur/> */}
+        <Switch>
           <Route exact path='/' component={HomePage} />
           <Route exact path='/project' component={ProjectPage} />
         </Switch>
--- src/pages/Project.page.jsx
+++ src/pages/Project.page.jsx
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import MetaTag from '../../components/MetaTag.component';
+import MetaTag from '../components/MetaTag.component';
 
 
 export default function HomePage() {
