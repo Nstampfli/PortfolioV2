4021ffec05c422c83671a1a737b29f142b50d033
Nstampfli <nathan.stampfli@hetic.net>
2020-08-09 19:59:53 +0200

    add work section just need add img

--- src/components/HomeWorks.component.jsx
+++ src/components/HomeWorks.component.jsx
@@ -1,16 +1,46 @@
 import React from 'react';
 import { breakpoints, colors } from '../styles/config/const';
 import styled from 'styled-components';
+import works from '../data/works.json';
 
 
 export default function HomeProjects() {
   return (
-    <TitleSection>
-      works<span>_</span>
-    </TitleSection>
+    <Container>
+        <TitleSection>
+          works<span>_</span>
+        </TitleSection>
+        {works.map((work, index) => {
+        return (
+          <ProjectContainer
+          key={index}
+          >
+            <ProjectImage>
+              <ImageBand  style={{ backgroundImage: `url(${require(`../assets/${work.image}`)})`}} ></ImageBand>
+              <ImageBand  style={{ backgroundImage: `url(${require(`../assets/${work.image}`)})`}} ></ImageBand>
+              <ImageBand  style={{ backgroundImage: `url(${require(`../assets/${work.image}`)})`}} ></ImageBand>
+              <ImageBand  style={{ backgroundImage: `url(${require(`../assets/${work.image}`)})`}} ></ImageBand>
+              <ImageBand  style={{ backgroundImage: `url(${require(`../assets/${work.image}`)})`}} ></ImageBand>
+            </ProjectImage>
+            <ProjectInfos>
+              <Title>{work.name}</Title>
+              <Info><span>/ </span>{work.infos}</Info>
+              <Goal>{work.goal}</Goal>
+            </ProjectInfos>
+          </ProjectContainer>
+        );
+        })}
+    </Container>
   )
 }
 
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
 const TitleSection = styled.h3`
   margin-top: 80px;
   text-align: center;
@@ -21,4 +51,108 @@ const TitleSection = styled.h3`
   span{
     color: ${colors.primaryColor}
   }
+`;
+const ProjectContainer = styled.div`
+  margin-top: 80px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  @media (min-width: ${breakpoints.tablet}) {
+    flex-direction: row;
+    width: 80%;
+    &:nth-child(2n){
+      flex-direction: row-reverse;
+    }
+  }
+`;
+const ProjectImage = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ImageBand = styled.div`
+  width: 50px;
+  height: 300px;
+  background: url('http://cdn-image.travelandleisure.com/sites/default/files/styles/1600x1000/public/asticou-azalea-garden.jpg?itok=q6aC8fly%27') no-repeat 0 0 fixed;
+  margin: 5px;
+  display: inline-block;
+  transition: all ease-in-out .4s;
+  &:hover{
+    transform: scale(1.1)
+  }
+    @media (min-width: ${breakpoints.tablet}) {
+      width: 75px;
+      height: 375px;
+      margin: 8px;
+    }
+    @media (min-width: ${breakpoints.laptop}) {
+      width: 100px;
+      height: 450px;
+      margin: 10px;
+    }
+  &:nth-child(1),:nth-last-child(1){
+    height: 200px;
+    @media (min-width: ${breakpoints.tablet}) {
+      height: 275px;
+    }
+    @media (min-width: ${breakpoints.laptop}) {
+      height: 350px;
+    }
+  }
+  &:nth-child(even){
+    height: 250px;
+    @media (min-width: ${breakpoints.tablet}) {
+    height: 325px;
+    }
+    @media (min-width: ${breakpoints.laptop}) {
+      height: 400px;
+    }
+  }
+`;
+const ProjectInfos = styled.div`
+  display: flex;
+  margin-top : 20px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center; 
+   @media (min-width: ${breakpoints.tablet}) {
+     max-width: 35%;
+    align-items: flex-start;
+    margin: 0px 50px
+    }
+`;
+
+const Title = styled.h2`
+  font-size: 50px;
+  text-transform: uppercase;
+  line-height: 60px;
+  text-align: center;
+  color: transparent;
+  -webkit-text-stroke: .6px ${colors.neutralWhite};
+  font-weight: 900;
+  @media (min-width: ${breakpoints.tablet}) {
+    font-size: 65px;
+    line-height: 80px;
+    text-align: initial;
+  }
+  @media (min-width: ${breakpoints.laptop}) {
+    font-size: 80px;
+    line-height: 100px;
+  }
+`;
+
+const Info = styled.p`
+  margin-top: 17px;
+  font-size: 18px;
+  span{
+    color: ${colors.primaryColor}
+  }
+`;
+const Goal = styled.p`
+  margin-top: 9px;
+  font-size: 18px;
+  color: ${colors.darkGrey}
 `;
